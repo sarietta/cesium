fa9d4216f9093f324ddf28c28316a3e16f038624
Sean Arietta <sarietta@eecs.berkeley.edu>
2012-10-03 13:19:59 -0700

    update

--- code/string/stringutils.cc
+++ code/string/stringutils.cc
@@ -1,3 +1,5 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include <string>
 #include <vector>
 
@@ -7,7 +9,7 @@ using std::string;
 using std::vector;
 
 namespace slib {
-
+  
   string StringUtils::Replace(const string& needle, const string& haystack, const string& replace) {
     size_t uPos = 0; 
     size_t uFindLen = needle.length(); 
@@ -52,7 +54,7 @@ namespace slib {
     arr.push_back(str.substr(k, i-k));
     return arr;
   }
-
+  
   void StringUtils::Replace(const string& needle, vector<string>* strings, const string& replace) {
     if (!strings) {
       return;
@@ -63,5 +65,77 @@ namespace slib {
       (*strings)[i].append(replaced);
     }
   }
-
+  
+  // Copyright 2002 The RE2 Authors.  All Rights Reserved.
+  // Use of this source code is governed by a BSD-style
+  // license that can be found in the LICENSE file.
+  void StringUtils::StringAppendV(string* dst, const char* format, va_list ap) {
+    // First try with a small fixed size buffer
+    char space[1024];
+    
+    // It's possible for methods that use a va_list to invalidate
+    // the data in it upon use.  The fix is to make a copy
+    // of the structure before using it and use that copy instead.
+    va_list backup_ap;
+    va_copy(backup_ap, ap);
+    int result = vsnprintf(space, sizeof(space), format, backup_ap);
+    va_end(backup_ap);
+    
+    if ((result >= 0) && (result < (int) sizeof(space))) {
+      // It fit
+      dst->append(space, result);
+      return;
+    }
+    
+    // Repeatedly increase buffer size until it fits
+    int length = sizeof(space);
+    while (true) {
+      if (result < 0) {
+	// Older behavior: just try doubling the buffer size
+	length *= 2;
+      } else {
+	// We need exactly "result+1" characters
+	length = result+1;
+      }
+      char* buf = new char[length];
+      
+      // Restore the va_list before we use it again
+      va_copy(backup_ap, ap);
+      result = vsnprintf(buf, length, format, backup_ap);
+      va_end(backup_ap);
+      
+      if ((result >= 0) && (result < length)) {
+	// It fit
+	dst->append(buf, result);
+	delete[] buf;
+	return;
+      }
+      delete[] buf;
+    }
+  }
+  
+  string StringUtils::StringPrintf(const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    string result;
+    StringAppendV(&result, format, ap);
+    va_end(ap);
+    return result;
+  }
+  
+  void StringUtils::SStringPrintf(string* dst, const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    dst->clear();
+    StringAppendV(dst, format, ap);
+    va_end(ap);
+  }
+  
+  void StringUtils::StringAppendF(string* dst, const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    StringAppendV(dst, format, ap);
+    va_end(ap);
+  }
+  
 }  // namespace slib
--- code/string/stringutils.h
+++ code/string/stringutils.h
@@ -1,6 +1,7 @@
 #ifndef __SLIB_STRING_STRINGUTILS_H__
 #define __SLIB_STRING_STRINGUTILS_H__
 
+#include <stdarg.h>
 #include <string>
 #include <vector>
 
@@ -10,9 +11,18 @@ using std::vector;
 namespace slib {
   class StringUtils {
   public:
-    static string Replace(const string& needle, const string& haystack, const string& replace);
-    static void Replace(const string& needle, vector<string>* strings, const string& replace);
+    static string Replace(const string& needle, const string& haystack, 
+			  const string& replace);
+    static void Replace(const string& needle, vector<string>* strings, 
+			const string& replace);
     static vector<string> Explode(const string& delimeter, const string& str);
+
+    // The following methods were copied from the RE2 library
+    // (code.google.com/p/re2.
+    static void StringAppendV(string* dst, const char* format, va_list ap);
+    static string StringPrintf(const char* format, ...);
+    static void SStringPrintf(string* dst, const char* format, ...);
+    static void StringAppendF(string* dst, const char* format, ...);
   };
 }  // namespace slib
 
