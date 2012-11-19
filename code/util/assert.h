b181ed41e9d5b7d1c5c9b074c3a8bbb458f452e4
Sean Arietta <sean.arietta@gmail.com>
2012-11-19 14:56:02 -0800

    update

--- code/util/assert.h
+++ code/util/assert.h
@@ -1,58 +1,63 @@
 #ifndef __SLIB_ASSERT_H__
 #define __SLIB_ASSERT_H__
 
-#include <glog/logging.h>
+#include <fstream>
+#include <iostream>
+
+using std::cerr;
+using std::cout;
+using std::endl;
 
 #define ASSERT_NOT_NULL(a) {						\
     if (a == NULL) {							\
-      LOG(ERROR) << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
-		 << ":: Assertion Failed! Expected "			\
-		 << #a << " not NULL";					\
+      cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
+	   << ":: Assertion Failed! Expected " << #a << " not NULL"	\
+	   << endl;							\
       exit(1);								\
     }									\
   }
 
 #define ASSERT_EQ(a, b) {						\
     if (a != b) {							\
-      LOG(ERROR) << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
+      cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
 	   << ":: Assertion Failed! Expected " << #a << " (" << a	\
-		 << ") = " << #b << " (" << b << ")";			\
+	   << ") = " << #b << " (" << b << ")" << endl;			\
       exit(1);								\
     }									\
   }
 
 #define ASSERT_LT(a, b) {						\
     if (a >= b) {							\
-      LOG(ERROR) << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
+      cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
 	   << ":: Assertion Failed! Expected " << #a << " (" << a	\
-		 << ") < " << #b << " (" << b << ")";			\
+	   << ") < " << #b << " (" << b << ")" << endl;			\
       exit(1);								\
     }									\
   }
 
 #define ASSERT_GT(a, b) {						\
     if (a <= b) {							\
-      LOG(ERROR) << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
+      cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
 	   << ":: Assertion Failed! Expected " << #a << " (" << a	\
-		 << ") > " << #b << " (" << b << ")";			\
+	   << ") > " << #b << " (" << b << ")" << endl;			\
       exit(1);								\
     }									\
   }
 
 #define ASSERT_LTE(a, b) {						\
     if (a > b) {							\
-      LOG(ERROR) << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
+      cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
 	   << ":: Assertion Failed! Expected " << #a << " (" << a	\
-		 << ") <= " << #b << " (" << b << ")";			\
+	   << ") <= " << #b << " (" << b << ")" << endl;		\
       exit(1);								\
     }									\
   }
 
 #define ASSERT_GTE(a, b) {						\
     if (a < b) {							\
-      LOG(ERROR) << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
+      cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__	\
 	   << ":: Assertion Failed! Expected " << #a << " (" << a	\
-		 << ") >= " << #b << " (" << b << ")";			\
+	   << ") >= " << #b << " (" << b << ")" << endl;		\
       exit(1);								\
     }									\
   }
