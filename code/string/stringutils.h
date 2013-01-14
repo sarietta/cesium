fc92714003809d167d65237214fff0bab5e7616b
Sean Arietta <sean.arietta@gmail.com>
2013-01-14 15:18:46 -0800

    update

--- code/string/stringutils.h
+++ code/string/stringutils.h
@@ -3,9 +3,11 @@
 
 #include <stdarg.h>
 #include <string>
+#include <sstream>
 #include <vector>
 
 using std::string;
+using std::stringstream;
 using std::vector;
 
 namespace slib {
@@ -16,6 +18,15 @@ namespace slib {
     static void Replace(const string& needle, vector<string>* strings, 
 			const string& replace);
     static vector<string> Explode(const string& delimeter, const string& str);
+
+    template <typename T>
+    static string StringUtils::Implode(const vector<T>& values, const string& delimeter) {
+      stringstream ss(stringstream::out);
+      for (int i = 0; i < (int) values.size(); i++) {
+	ss << values[i] << delimeter;
+      }
+      return ss.str();
+    }
 
     // The following methods were copied from the RE2 library
     // (code.google.com/p/re2.
