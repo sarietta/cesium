43eccf558fa28373aed30cf4ed8f82c97f97890a
Sean Arietta <sean.arietta@gmail.com>
2013-10-08 18:58:00 -0700

    MATLAB sparse matrix support fully implemented. Tests pass and the Model class in SVM library can now load LIBSVM models directly using the sparse matrix support

--- code/svm/model.h
+++ code/svm/model.h
@@ -2,7 +2,14 @@
 #define __SLIB_SVM_MODEL_H__
 
 #include <common/scoped_ptr.h>
+#include <common/types.h>
 #include <vector>
+
+namespace slib {
+  namespace util {
+    class MatlabMatrix;
+  }
+}
 
 namespace slib {
   namespace svm {
@@ -37,31 +44,9 @@ namespace slib {
       
       Model() : weights(NULL), num_weights(0), rho(0.0f), first_label(1.0f)
 	      , threshold(0.0f), num_positives(0), num_negatives(0) {}
-#if 0
-      Model(const slib::util::MatlabMatrix& libsvm_model) {
-	const MatlabMatrix& SVs = libsvm_model.GetStructField("SVs");
-	const Pair<int> dimensions = SVs.GetDimensions();
 
-	const int num_SVs = dimensions.x;
-	num_weights = dimensions.y;
-	
-	weights.reset(new float[num_weights]);
-	for (int i = 0; i < num_weights; i++) {
-	  weights[i] = 0.0f;
-	}
-	for (int j = 0; j < num_SVs; j++) {
-	  int x_index = 0;
-	  while (SVs(j,x_index).index != -1) {
-	    const int feature_dimension_index = libmodel->SV[j][x_index].index - 1;
-	    model->weights[feature_dimension_index] += libmodel->sv_coef[0][j] * libmodel->SV[j][x_index].value;
-	    x_index++;
-	  }
-	}
-	
-	model->rho = libmodel->rho[0];
-	model->first_label = libmodel->label[0];
-      }
-#endif      
+      Model(const slib::util::MatlabMatrix& libsvm_model);
+
       Model& operator=(const Model& rhs) {
 	num_weights = rhs.num_weights;
 	weights.reset(new float[num_weights]);
