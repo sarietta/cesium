0a4ec0227c594fb894d26cfa97946e17a658f701
Sean Arietta <sean.arietta@gmail.com>
2014-10-30 15:23:46 -0700

    Added explicit namespace to stringutils signatures

--- code/string/stringutils.h
+++ code/string/stringutils.h
@@ -6,21 +6,20 @@
 #include <sstream>
 #include <vector>
 
-using std::string;
 using std::stringstream;
 using std::vector;
 
 namespace slib {
   class StringUtils {
   public:
-    static string Replace(const string& needle, const string& haystack, 
-			  const string& replace);
-    static void Replace(const string& needle, vector<string>* strings, 
-			const string& replace);
-    static vector<string> Explode(const string& delimeter, const string& str);
+    static std::string Replace(const std::string& needle, const std::string& haystack, 
+			       const std::string& replace);
+    static void Replace(const std::string& needle, vector<std::string>* strings, 
+			const std::string& replace);
+    static vector<std::string> Explode(const std::string& delimeter, const std::string& str);
 
     template <typename T>
-    static string Implode(const vector<T>& values, const string& delimeter) {
+    static std::string Implode(const vector<T>& values, const std::string& delimeter) {
       stringstream ss(stringstream::out);
       for (int i = 0; i < (int) values.size(); i++) {
 	ss << values[i] << delimeter;
@@ -30,10 +29,10 @@ namespace slib {
 
     // The following methods were copied from the RE2 library
     // (code.google.com/p/re2.
-    static void StringAppendV(string* dst, const char* format, va_list ap);
-    static string StringPrintf(const char* format, ...);
-    static void SStringPrintf(string* dst, const char* format, ...);
-    static void StringAppendF(string* dst, const char* format, ...);
+    static void StringAppendV(std::string* dst, const char* format, va_list ap);
+    static std::string StringPrintf(const char* format, ...);
+    static void SStringPrintf(std::string* dst, const char* format, ...);
+    static void StringAppendF(std::string* dst, const char* format, ...);
   };
 }  // namespace slib
 
